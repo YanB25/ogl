66cac341f18da6f1799e7720d8e2ca4a815789ce
YanB25 <yanb25@mail2.sysu.edu.cn>
2018-05-04 02:49:58 +0800

    init

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -4,40 +4,51 @@
 #include <GL/glew.h>
 
 #include <GLFW/glfw3.h>
-GLFWwindow* window;
+GLFWwindow *window;
 
 #include <glm/glm.hpp>
 using namespace glm;
-
-int main( void )
+static void glfw_error_callback(int error, const char* description);
+static void key_call_back(GLFWwindow* windowk, int key, int scanCode, int action, int mod);
+int main(void)
 {
 	// Initialise GLFW
-	if( !glfwInit() )
+	if (!glfwInit())
 	{
-		fprintf( stderr, "Failed to initialize GLFW\n" );
+		fprintf(stderr, "Failed to initialize GLFW\n");
 		getchar();
 		return -1;
 	}
+	// set error callback function, that only print error msg.
+	glfwSetErrorCallback(glfw_error_callback);
+	
 
+	// init stuffs
 	glfwWindowHint(GLFW_SAMPLES, 4);
-	glfwWindowHint(GLFW_RESIZABLE,GL_FALSE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+	// required GLFW version 3.3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Open a window and create its OpenGL context
-	window = glfwCreateWindow( 1024, 768, "Playground", NULL, NULL);
-	if( window == NULL ){
-		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
+	window = glfwCreateWindow(1024, 768, "Playground", NULL, NULL);
+	if (window == NULL)
+	{
+		fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
 		getchar();
 		glfwTerminate();
 		return -1;
 	}
+	// set key callback that do nothing
+	glfwSetKeyCallback(window, key_call_back);
+
 	glfwMakeContextCurrent(window);
 
 	// Initialize GLEW
-	if (glewInit() != GLEW_OK) {
+	if (glewInit() != GLEW_OK)
+	{
 		fprintf(stderr, "Failed to initialize GLEW\n");
 		getchar();
 		glfwTerminate();
@@ -50,7 +61,8 @@ int main( void )
 	// Dark blue background
 	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
 
-	do{
+	do
+	{
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		// Draw nothing, see you in tutorial 2 !
@@ -60,8 +72,8 @@ int main( void )
 		glfwPollEvents();
 
 	} // Check if the ESC key was pressed or the window was closed
-	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
-		   glfwWindowShouldClose(window) == 0 );
+	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+		   glfwWindowShouldClose(window) == 0);
 
 	// Close OpenGL window and terminate GLFW
 	glfwTerminate();
@@ -69,3 +81,12 @@ int main( void )
 	return 0;
 }
 
+static void glfw_error_callback(int error, const char* description) {
+	fprintf(stderr, "Error in glfw, msg is: %s\n", description);
+	fprintf(stderr, "Error in glfw, code is: %d\n", error);
+}
+
+static void key_call_back(GLFWwindow* windowk, int key, int scanCode, int action, int mod) {
+	// printf("key callback, key %d, scancode %d, action %d, mod %d \n", key, scanCode, action, mod);
+	printf("key pressed\n");
+}
