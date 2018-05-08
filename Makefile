a7d76e8f7930d4c376c75aecf2cae43dbf3ac835
YanB25 <yanb25@mail2.sysu.edu.cn>
2018-05-08 08:31:19 +0800

    finish source all. to delete unnecessary

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
-all:
-	@echo "Please READ Tutorial 1 and don't build the tutorials yourself ! Use CMake instead. If you have a problem, read the FAQ"
+default_target: all
+all: helloworld cubic
+helloworld:
+	cd helloworld/ && make
+	cd ../
+cubic:
+	cd cubic/ && make 
+	cd ../
--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -15,81 +15,22 @@ static void glfw_error_callback(int error, const char* description);
 static void key_call_back(GLFWwindow* windowk, int key, int scanCode, int action, int mod);
 
 static const GLfloat g_vertex_buffer_data[] = {
-    -1.0f,-1.0f,-0.6f, // triangle 1 : begin
-    -1.0f,-1.0f, 0.6f,
-    -1.0f, 1.0f, 0.6f, // triangle 1 : end
-    1.0f, 1.0f,-1.0f, // triangle 2 : begin
-    -1.0f,-1.0f,-0.6f,
-    -1.0f, 1.0f,-0.6f, // triangle 2 : end
-    1.0f,-1.0f, 1.0f, //tr3 : begin
-    -1.0f,-1.0f,-0.6f,
-    1.0f,-1.0f,-1.0f, //tri 3 : end
-    1.0f, 1.0f,-1.0f,
-    1.0f,-1.0f,-1.0f,
-    -1.0f,-1.0f,-0.6f,
-    -1.0f,-1.0f,-0.6f,
-    -1.0f, 1.0f, 0.6f,
-    -1.0f, 1.0f,-0.6f,
-    1.0f,-1.0f, 1.0f,
-    -1.0f,-1.0f, 0.6f,
-    -1.0f,-1.0f,-0.6f,
-    -1.0f, 1.0f, 0.6f,
-    -1.0f,-1.0f, 0.6f,
-    1.0f,-1.0f, 1.0f,
-    1.0f, 1.0f, 1.0f,
-    1.0f,-1.0f,-1.0f,
-    1.0f, 1.0f,-1.0f,
-    1.0f,-1.0f,-1.0f,
-    1.0f, 1.0f, 1.0f,
-    1.0f,-1.0f, 1.0f,
-    1.0f, 1.0f, 1.0f,
-    1.0f, 1.0f,-1.0f,
-    -1.0f, 1.0f,-0.6f,
-    1.0f, 1.0f, 1.0f,
-    -1.0f, 1.0f,-0.6f,
-    -1.0f, 1.0f, 0.6f,
-    1.0f, 1.0f, 1.0f,
-    -1.0f, 1.0f, 0.6f,
-    1.0f,-1.0f, 1.0f
+	-0.5f, 0.5f,
+	0.5f, 0.5f,
+	-0.5f, -0.5f,
+
+	0.5f, -0.5f,
+	0.5f, 0.5f,
+	-0.5f, -0.5f
 };
 
 static const GLfloat g_color_buffer_data[] = {
-    0.583f,  0.771f,  0.014f,
-    0.609f,  0.115f,  0.436f,
-    0.327f,  0.483f,  0.844f,
-    0.822f,  0.569f,  0.201f,
-    0.435f,  0.602f,  0.223f,
-    0.310f,  0.747f,  0.185f,
-    0.597f,  0.770f,  0.761f,
-    0.559f,  0.436f,  0.730f,
-    0.359f,  0.583f,  0.152f,
-    0.483f,  0.596f,  0.789f,
-    0.559f,  0.861f,  0.639f,
-    0.195f,  0.548f,  0.859f,
-    0.014f,  0.184f,  0.576f,
-    0.771f,  0.328f,  0.970f,
-    0.406f,  0.615f,  0.116f,
-    0.676f,  0.977f,  0.133f,
-    0.971f,  0.572f,  0.833f,
-    0.140f,  0.616f,  0.489f,
-    0.997f,  0.513f,  0.064f,
-    0.945f,  0.719f,  0.592f,
-    0.543f,  0.021f,  0.978f,
-    0.279f,  0.317f,  0.505f,
-    0.167f,  0.620f,  0.077f,
-    0.347f,  0.857f,  0.137f,
-    0.055f,  0.953f,  0.042f,
-    0.714f,  0.505f,  0.345f,
-    0.783f,  0.290f,  0.734f,
-    0.722f,  0.645f,  0.174f,
-    0.302f,  0.455f,  0.848f,
-    0.225f,  0.587f,  0.040f,
-    0.517f,  0.713f,  0.338f,
-    0.053f,  0.959f,  0.120f,
-    0.393f,  0.621f,  0.362f,
-    0.673f,  0.211f,  0.457f,
-    0.820f,  0.883f,  0.371f,
-    0.982f,  0.099f,  0.879f
+	1.0f, 1.0f, 1.0f,
+	1.0f, 1.0f, 1.0f,
+	1.0f, 1.0f, 1.0f,
+	1.0f, 1.0f, 1.0f,
+	1.0f, 1.0f, 1.0f,
+	1.0f, 1.0f, 1.0f
 };
 
 int main(void)
@@ -115,7 +56,7 @@ int main(void)
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Open a window and create its OpenGL context
-	window = glfwCreateWindow(1024, 768, "cubic", NULL, NULL);
+	window = glfwCreateWindow(700, 700, "helloworld", NULL, NULL);
 	if (window == NULL)
 	{
 		fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
@@ -164,23 +105,6 @@ int main(void)
 	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
 
-
-	//matrix transformation
-	glm::mat4 Projection = glm::perspective(glm::radians(45.0f), (float) 4 / (float) 3, 0.1f, 100.0f);
-	glm::mat4 View = glm::lookAt(
-		glm::vec3(3, 3, 6),
-		glm::vec3(0, 0, 0),
-		glm::vec3(0, 1, 0)
-	);
-
-	glm::mat4 Model = glm::mat4(1.0f);
-
-	glm::mat4 mvp = Projection * View * Model;
-
-
-
-
-
 	do
 	{
 		glClear(GL_COLOR_BUFFER_BIT);
@@ -192,10 +116,10 @@ int main(void)
 		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 		glVertexAttribPointer(
 			posAttri,
-			3,
+			2,
 			GL_FLOAT,
 			GL_FALSE,
-			3 * sizeof(GLfloat),
+			2 * sizeof(GLfloat),
 			0
 		);
 
@@ -204,32 +128,15 @@ int main(void)
 		glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
 		glVertexAttribPointer(
 			colorAttri,
-			3,                                // size
-			GL_FLOAT,                         // type
-			GL_FALSE,                         // normalized?
-			0,                                // stride
-			(void*)0                          // array buffer offset
+			3,
+			GL_FLOAT,
+			GL_FALSE,
+			0,
+			(void*)0
 		);
-
-
-		// GLint colorAttri = glGetAttribLocation(programID, "color");
-		// glEnableVertexAttribArray(colorAttri);
-		// glVertexAttribPointer(
-		// 	colorAttri,
-		// 	3,
-		// 	GL_FLOAT,
-		// 	GL_FALSE,
-		// 	6 * sizeof(GLfloat),
-		// 	(void*)(sizeof(GLfloat) * 3)
-		// );
-
-		//TODO: should they be in the loop ? why?
-		GLuint MatrixID = glGetUniformLocation(programID, "MVP");
-		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &mvp[0][0]);
 	
 		// glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-		glDrawArrays(GL_TRIANGLES, 0, 12*3); 
-		//glDisableVertexAttribArray(6);
+		glDrawArrays(GL_TRIANGLES, 0, 6); 
 		// Swap buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
